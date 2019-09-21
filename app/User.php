31a02f903bf25e7370259478a62a0d81e529817f
Santhosh Suryavanshi <santhusurya@gmail.com>
2019-09-22 02:42:36 +0530

    Configured Vue scaffolding. Configured auto UUID generation for Primary Key for all Models

--- app/User.php
+++ app/User.php
@@ -5,10 +5,26 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Str;
 
 class User extends Authenticatable
 {
     use Notifiable;
+
+    public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+
+            $model->{$model->getKeyName()} = (string) Str::uuid();
+            //$model->id = Str::uuid();
+        });
+    }
+
+
 
     /**
      * The attributes that are mass assignable.
